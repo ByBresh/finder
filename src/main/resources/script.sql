300917edd05f58fb95e879ac0b3ba9fa86780008
ByBresh <116611676+ByBresh@users.noreply.github.com>
2025-01-27 18:04:50 +0100

    Changes to Database

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -8,6 +8,5 @@ CREATE TABLE users (
     name VARCHAR(50) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    profile_picture LONGBLOB NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    profile_picture LONGBLOB NOT NULL
 );
